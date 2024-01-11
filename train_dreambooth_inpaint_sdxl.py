9044bc207e9b75f10aa85887f9adc72f0fa62a1c
Aleksey Korshuk <ale-kor02@mail.ru>
2024-01-11 19:28:07 +0300

    updates

--- train_dreambooth_inpaint_sdxl.py
+++ train_dreambooth_inpaint_sdxl.py
@@ -106,7 +106,7 @@ def get_preprocessed_image(initial_image, mask_image):
     return combined_image
 
 
-prepare_mask_and_masked_image = prepare_mask_and_masked_image_v1
+prepare_mask_and_masked_image = prepare_mask_and_masked_image_v0
 
 
 # generate random masks
