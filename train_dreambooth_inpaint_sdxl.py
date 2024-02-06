8986748cc861279b837d5395579843bff0118f56
Aleksey Korshuk <ale-kor02@mail.ru>
2024-02-06 21:45:28 +0300

    updates

--- train_dreambooth_inpaint_sdxl.py
+++ train_dreambooth_inpaint_sdxl.py
@@ -350,7 +350,7 @@ def parse_args():
     parser.add_argument(
         "--checkpointing_steps",
         type=int,
-        default=1000,
+        default=5000,
         help=(
             "Save a checkpoint of the training state every X updates. These checkpoints can be used both as final"
             " checkpoints in case they are better than the last checkpoint and are suitable for resuming training"
