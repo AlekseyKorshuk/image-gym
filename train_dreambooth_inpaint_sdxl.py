daca7d658923ac21b819b641775a6d711cafa1b5
Aleksey Korshuk <ale-kor02@mail.ru>
2024-02-06 12:28:05 +0300

    updates

--- train_dreambooth_inpaint_sdxl.py
+++ train_dreambooth_inpaint_sdxl.py
@@ -765,9 +765,9 @@ def main():
             # "unet_added_conditions": {"text_embeds": add_text_embeds, "time_ids": add_time_ids},
         }
 
-    shuffled_train_dataset = ShufflerIterDataPipe(train_dataset, buffer_size=args.train_batch_size)
+    # shuffled_train_dataset = ShufflerIterDataPipe(train_dataset, buffer_size=args.train_batch_size)
     train_dataloader = torch.utils.data.DataLoader(
-        shuffled_train_dataset, batch_size=args.train_batch_size, shuffle=True, collate_fn=collate_fn,
+        train_dataset, batch_size=args.train_batch_size, shuffle=True, collate_fn=collate_fn,
         drop_last=True
     )
 
@@ -788,6 +788,7 @@ def main():
     unet, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
         unet, optimizer, train_dataloader, lr_scheduler
     )
+    torch.utils.data.graph_settings.apply_shuffle_settings(train_dataloader.dataset, shuffle=True)
     if args.use_ema:
         ema_unet.to(accelerator.device)
     # print(f"After accelerate prepare: {train_dataloader.dataset._shuffle_enabled}")
