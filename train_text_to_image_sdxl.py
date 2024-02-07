574b37c7cf8002922456e2b49bc9dfc45fe27905
Aleksey Korshuk <ale-kor02@mail.ru>
2024-02-07 19:49:19 +0300

    updates

--- train_text_to_image_sdxl.py
+++ train_text_to_image_sdxl.py
@@ -897,14 +897,14 @@ def main(args):
     torch.cuda.empty_cache()
 
     def collate_fn(examples):
-        # model_input = torch.stack([torch.tensor(example["model_input"]) for example in examples])
+        pixel_values = torch.stack([torch.tensor(example["pixel_values"]) for example in examples])
         original_sizes = [example["original_sizes"] for example in examples]
         crop_top_lefts = [example["crop_top_lefts"] for example in examples]
         prompt_embeds = torch.stack([torch.tensor(example["prompt_embeds"]) for example in examples])
         pooled_prompt_embeds = torch.stack([torch.tensor(example["pooled_prompt_embeds"]) for example in examples])
 
         return {
-            # "model_input": model_input,
+            "pixel_values": pixel_values,
             "prompt_embeds": prompt_embeds,
             "pooled_prompt_embeds": pooled_prompt_embeds,
             "original_sizes": original_sizes,
